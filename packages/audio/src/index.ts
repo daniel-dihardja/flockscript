05dd8163b43128a6c81c37af8db74ef280b3a566
Daniel Dihardja <daniel@dihardja.de>
2026-02-24 14:16:57 +0100

    chore(audio): rely on exports and ts source

--- packages/audio/src/index.ts
+++ packages/audio/src/index.ts
@@ -1,6 +1,4 @@
 import audioEngine from "./audio-engine";
 import PatchBuilder from "./patch-builder";
-import { patches, patchNames } from "./patches";
-
-export { audioEngine, PatchBuilder, patches, patchNames };
+export { audioEngine, PatchBuilder };
 export default { audioEngine, PatchBuilder };
