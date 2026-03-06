fd6751d03cc0085cdb8b69552357da8a6e25f70c
Daniel Dihardja <daniel@dihardja.de>
2026-03-06 13:49:13 +0100

    fix build

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui", "@workspace/visuals"],
+  transpilePackages: ["@workspace/ui"],
 };
 
 export default nextConfig;
